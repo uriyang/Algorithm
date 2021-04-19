7384a10f8274ff810cbaf805ad1ee738b2801127
uriyang <girlstoryk@gmail.com>
2021-04-19 23:38:12 +0900

    Sec4: 4-4 졸업선물

---
+++ Section4/4.js
@@ -0,0 +1,34 @@
+// 선생님은 올해 졸업하는 반 학생들에게 졸업선물을 주려고 합니다.
+// 학생들에게 인터넷 쇼핑몰에서 각자 원하는 상품을 골라 그 상품의 가격과 배송비를 제출하라 고 했습니다. 선생님이 가지고 있는 예산은 한정되어 있습니다.
+// 현재 예산으로 최대 몇 명의 학생에게 선물을 사줄 수 있는지 구하는 프로그램을 작성하세요. 선생님은 상품 하나를 50% 할인해서(반 가격) 살 수 있는 쿠폰을 가지고 있습니다. 배송비는 할인에 포함되지 않습니다.
+function solution(m, product) {
+  let answer = 0;
+  let useCoupon = false;
+  product.sort((a, b) => b[0] - a[0]);
+
+  function calculator(price, total) {
+    if (price <= 10 && total >= price) {
+      m -= price;
+      answer++;
+      return true;
+    }
+  }
+
+    const gift = product[i];
+    if (!useCoupon) {
+      if (calculator(gift[0] / 2 + gift[1], m)) useCoupon = true;
+    } else {
+      calculator(gift[0] + gift[1], m);
+    }
+  }
+  return answer;
+}
+
+let arr = [
+  [6, 6],
+  [2, 2],
+  [4, 3],
+  [4, 5],
+  [10, 3],
+];
+console.log(solution(28, arr));
