e43df1e92f0c984c5893a572849584a9367c042d
uriyang <girlstoryk@gmail.com>
2021-04-21 00:22:34 +0900

    Sec5: 5-1 / 코드 보완

--- Section5/1.js
+++ Section5/1.js
@@ -1,6 +1,6 @@
 // 오름차순으로 정렬이 된 두 배열이 주어지면 두 배열을 오름차순으로 합쳐 출력하는 프로그램 을 작성하세요.
 function solution(arr1, arr2) {
-  let answer = [...arr1, ...arr2].sort();
+  let answer = [...arr1, ...arr2].sort((a, b) => a - b);
   return answer;
 }
 
