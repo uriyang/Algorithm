8a0bb813851e576db839407cbf5fb7a45be7ae06
uriyang <girlstoryk@gmail.com>
2021-05-11 01:41:00 +0900

    Sec7: 7-1 선택정렬

---
+++ Section7/1.js
@@ -0,0 +1,15 @@
+// N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요. 정렬하는 방법은 선택정렬입니다.
+function solution(arr) {
+  let answer = arr;
+  for (let i = 0; i < arr.length; i++) {
+    let idx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[idx]) idx = j;
+    }
+    [arr[i], arr[idx]] = [arr[idx], arr[i]];
+  }
+  return answer;
+}
+
+let arr = [13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
