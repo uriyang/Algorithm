0c4621b0990a8d51cfdb7777fc7e689d07af4886
uriyang <girlstoryk@gmail.com>
2021-05-21 02:16:40 +0900

    Sec7: 7-10 이분검색

---
+++ Section7/10.js
@@ -0,0 +1,11 @@
+// 임의의 N개의 숫자가 입력으로 주어집니다. N개의 수를 오름차순으로 정렬한 다음 N개의 수 중 한 개의 수인 M이 주어지면 이분검색으로 M이 정렬된 상태에서 몇 번째에 있는지 구하는 프로그램을 작성하세요. 단 중복값은 존재하지 않습니다.
+function solution(target, arr) {
+  let answer;
+  arr.sort((a, b) => a - b);
+  answer = arr.findIndex((item) => item === target) + 1;
+
+  return answer;
+}
+
+let arr = [23, 87, 65, 12, 57, 32, 99, 81];
+console.log(solution(32, arr));
